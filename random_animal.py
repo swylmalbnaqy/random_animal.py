d5fca335f27f25430d9a425e048399e179ac95c5
swylmalbnaqy <129783325+swylmalbnaqy@users.noreply.github.com>
2023-04-18 17:44:25 +0300

    Create random_animal.py

---
+++ random_animal.py
@@ -0,0 +1,9 @@
+# random_animal.py
+
+import random
+
+animals = ["кошка", "собака", "крыса", "хомяк", "рыба", "попугай"]
+
+random_animal = random.choice(animals)
+
+print("Случайное животное: ", random_animal)
